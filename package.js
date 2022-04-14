8b36d5a821ccabfefe85498116034d77fc4aa67b
idyllim <idyllim@gmail.com>
2022-04-14 14:37:07 +0800

    Create package.js

---
+++ package.js
@@ -0,0 +1,16 @@
+{
+  "name": "insomnia-plugin-request-runner",
+  "version": "1.0.0",
+  "description": "test request with cvs data.",
+  "license": "MIT",
+  "main": "main.js",
+  "author": "Ken",
+  "repository": {
+      "type": "git",
+      "url": "https://github.com/idyllim/insomnia-plugin-request-runner"
+  },
+  "insomnia": {
+      "name": "request-runner",
+      "description": "test request with cvs data."
+  }
+}
